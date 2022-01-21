f1e704773cc4f7a4898ad74eab0511fbf263da87
Juno <junonguyen27@gmail.com>
2022-01-20 18:23:20 -0600

    inquirer function finished

--- index.js
+++ index.js
@@ -1,90 +1,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const generateTeam = require('./src/page-template');
 
-const questions = [
-    {
-        type: 'list',
-        message: 'Are you an employee?',
-        name: 'employed',
-        choices: ['Yes','No'],
-    }
-];
-
-const employeeQuestions = [
+const team = [
     {
         type: 'input',
         message: 'What is your name?',
         name: 'name',
     },
     {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your ID?',
+        name: 'id',
+    },
+    {
         type: 'list',
         message: 'What is your position?',
         name: 'position',
-        choices: ['Manager','Engineer','Intern']
-    }
-];
-
-const managerQuestions = [
+        choices: ['Manager','Engineer','Intern'],
+    },
     {
         type: 'input',
         message: 'What is your office number?',
-        name: 'office'
-    }
-];
-
-const engineerQuestions = [
+        name: 'office',
+        when: ((response) => response.position.toLowerCase() === 'manager'),
+    },
     {
         type: 'input',
         message: 'What is your GitHub username?',
-        name: 'github'
-    }
-];
-
-const internQuestions = [
+        name: 'github',
+        when: ((response) => response.position.toLowerCase() === 'engineer'),
+    },
     {
         type: 'input',
         message: 'What is your school?',
-        name: 'school'
+        name: 'school',
+        when: ((response) => response.position.toLowerCase() === 'intern'),
     }
 ];
 
-// const printCard = inquirer
-//     .prompt(questions)
-//     .then((response) => {
-//         if(response.employed.toLowerCase() !== 'yes') {
-//            return;
-//         } else {
-//             //WRITE PROMPT FOR EMPLOYEE INFORMATION
-//             //WRITE FUNCTION TO PRINT ID CARD
-//             inquirer
-//                 .prompt(employeeQuestions)
-//                 .then((employeeResponse) => {
-//                     if(employeeResponse.position.toLowerCase() === 'manager') {
-//                         inquirer
-//                             .prompt(managerQuestions)
-//                             .then((manangerResponse) => {
-//                                 return manangerResponse;
-//                             });
-//                     } else if(employeeResponse.position.toLowerCase() === 'engineer') {
-//                         inquirer
-//                             .prompt(engineerQuestions)
-//                             .then((engineerResponse) => {
-//                                 return engineerResponse;
-//                             });
-//                     } else if(employeeResponse.position.toLowerCase() === 'intern') {
-//                         inquirer
-//                             .prompt(internQuestions)
-//                             .then((internQuestions) => {
-//                                 return internQuestions;
-//                             });
-//                     }
-//                 })
-//                 fs.writeFile('test', manangerResponse, (err) => {
-//                     err ? console.log(err) : console.log('success')
-//                 })
-//         }
-//     })
-
-    const printCard = inquirer
-    .prompt(questions)
+const printCard = inquirer
+    .prompt(team)
+    .then((response) => {
+        console.log(response)
+        if(response.position.toLowerCase() === 'manager') {
+            var manager = generateTeam(response);
+            console.log(manager);
+        }
+    })
