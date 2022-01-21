16a9a494386c5410f2d9718c358328224a7e57c5
Juno <junonguyen27@gmail.com>
2022-01-20 19:09:05 -0600

    trying to send right data to page-template.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const printCard = inquirer
         } else if (response.position.toLowerCase() === "manager") {
             newMember = new Manager(response.name, response.id, response.email);
         }
-        var test = generateTeam(response);
+        var test = generateTeam(newMember);
         fs.writeFile('test.html', test, (err) => {
             err ? console.log(err) : console.log('Team created!')
         })
